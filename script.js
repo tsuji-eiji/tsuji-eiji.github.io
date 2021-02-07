55608c2611928a9ae8b777ac9ffce29abe810344
tsuji-eiji <eiji.rock24@gmail.com>
2021-02-07 10:49:09 +0900

    no message

---
+++ script.js
@@ -0,0 +1,15 @@
+'use strict'
+
+document.getElementById('menu').addEventListener('click', function () {
+	
+	document.getElementById('nav').classList.toggle('nav');
+//	const btn = document.getElementById('menu');
+//	const menu = btn.firstElementChild.textContent;
+//	if (menu === 'MENU') {
+//		//MENUをCLOSEに
+//		btn.innerHTML = '<h2>CLOSE</h2>';
+//	} else if (menu === 'CLOSE') {
+//		//CLOSEをMENUに
+//		btn.innerHTML = '<h2>MENU</h2>';
+//	}
+},false);
